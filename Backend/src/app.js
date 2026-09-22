761782d6819d6e1245ed7313f78b8945f751648b
Prajapati Hitesh <hiteshprajapat1752000@gmail.com>
2026-09-22 14:51:07 +0530

    backend Core

--- Backend/src/app.js
+++ Backend/src/app.js
@@ -12,7 +12,7 @@ import recruiterRouter from "./routes/recruiter.route.js";
 
 const app = express();
 
-app.use(cors({origin: "http://localhost:5173",credentials: true}));
+app.use(cors({origin: process.env.CLIENT_URL,credentials: true}));
 // Middleware
 app.use(
   express.json({
